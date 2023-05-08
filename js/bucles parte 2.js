b519477ba6ebff140a5d28c4e406dad4d6469416
sebastianVH <114303713+sebastianVH@users.noreply.github.com>
2023-05-08 19:17:56 -0300

    clases 8 de mayo

---
+++ js/bucles parte 2.js
@@ -0,0 +1,36 @@
+//? bucles FOR  en arrays y objetos
+
+//*For-each: recorre CADA elemento de un array,  y aplica alguna FUNCION sobre este
+
+let array_frutas = ["Banana","Manzana","Pera","Uva"]
+
+array_frutas.forEach(fruta => {
+    console.log(fruta + " Debo comprar 1kg");
+})
+
+console.log(array_frutas);
+
+//*For-in: el forIn, nos sirve para recorrer las propiedades encontradas en nuestros objetos
+
+const datos = {nombre: "Seba", edad: 35, carrera: "Desarrollador FullStack", hobbies:["Cantar","Jugar Juegos en linea","Leer","Tomar mate"]};
+
+console.log(datos["edad"])
+
+for (let propiedad in datos ){
+    console.log(propiedad); //mostramos como se llama la propiedad
+    console.log(datos[propiedad]);//mostrar cual es el valor de nuestra propiedad
+}
+
+//*For-of: podemos recorrer un array o un STRING
+
+const diccionario = ["a", "b", "c","d","e","f"];
+const palabra = "Global Academy"
+
+for (let letra of palabra) {
+    console.log(letra);
+}
+
+for (let hobby of datos.hobbies){
+    console.log("Al profe Seba le gusta "+hobby);
+}
+
--- js/bucles.js
+++ js/bucles.js
@@ -31,8 +31,8 @@ console.log(lista_cervezas);
 
 
 let lunas_saturno = 53 //esta variable es la de la respuesta correcta
-let numero_arriesgado; // es mi variable para COMPARAR.
-
+//let numero_arriesgado; // es mi variable para COMPARAR.
+/*
 while (numero_arriesgado != lunas_saturno) { //!EVALUO 2 VARIABLES DIFERENTES PARA MI CONDICION.
     numero_arriesgado = prompt(" Adivine la cantidad de lunas: ")
     if (numero_arriesgado > lunas_saturno) {
@@ -42,9 +42,7 @@ while (numero_arriesgado != lunas_saturno) { //!EVALUO 2 VARIABLES DIFERENTES PA
     }
 }
 alert("Felicitaciones acertaste!!!")
-
-
-
+*/
 
 //? BUCLE FOR: se utiliza para ITERAR SOBRE ELEMENTOS LOS CUALES SEAN ITERABLES.
 
@@ -62,11 +60,76 @@ for (let i = 0; i < lista_frutas.length; i++) {
     console.log(lista_frutas[i])
 } //va a ejecutarse , segun un rango de veces que nosotros le indiquemos, y va a utilizar NUMEROS para la iteracion
 
+
+
 /*TODO: 
-*Crear un bucle WHILE, que recorra de un rango numerico desde el 1 al 10. ASEGURARSE QUE NO ENTRE EN UN BUCLE INFINITO
+*Crear un bucle WHILE, que recorra de un rango numérico desde el 1 al 10. ASEGURARSE QUE NO ENTRE EN UN BUCLE INFINITO
 
 *Crear un bucle WHILE, que pida al usuario que adivine el numero secreto. El usuario puede ingresar los numeros por la terminal.
 este while, debe TERMINAR si el usuario acierta el valor. Sino, se repite de manera infinita
 
 *Crear una lista de animales , com 6 animales, e imprimir por consola, un mensaje que diga "Animal *ej 1* es *nombre_animal*"
 */ 
+
+
+//* Ejecicio 1)
+
+let contador = 1
+
+while(contador <= 10) {
+    console.log(contador);
+    contador++
+}
+
+//* Ejercicio 2)
+
+let numero_adivinar = 30
+let numero_arriesgado //variable DECLARADA pero no INICIALIZADA => la variable existe, pero NO TIENE VALOR
+console.log(numero_arriesgado);
+
+/*
+while(numero_arriesgado != numero_adivinar){
+    numero_arriesgado = prompt("Ingrese el numero a Adivinar!!")
+    if (numero_arriesgado < numero_adivinar) {
+        alert("El numero arriesgado es MAS BAJO que el numero secreto")
+    } else if ( numero_arriesgado > numero_adivinar) {
+        alert("El numero arriesgado es MAS ALTO que el numero secreto")
+    } else {
+        alert(`Acertaste!! El numero secreto era ${numero_adivinar}` )
+    }
+}
+*/
+
+//* ejercicio 3)
+
+//                       0     1        2        3         4        5
+let lista_animales = ["Perro","Gato","Leon","Canario","Ballena","Tortuga"]
+
+for(let i = 0;i < lista_animales.length;i++) {
+    console.log(`El animal ${i+1} es ${lista_animales[i]}`);
+}
+
+
+
+
+
+
+
+
+
+
+//? For in
+const objeto = {a: 1, b: 2, c: 3};
+
+for (let propiedad in objeto) {
+  console.log(`${propiedad}: ${objeto[propiedad]}`);
+}
+
+//?For Of
+const letras = ["a", "b", "c"];
+
+for (let letra of letras) {
+  console.log(letra);
+}
+
+//?For Each
